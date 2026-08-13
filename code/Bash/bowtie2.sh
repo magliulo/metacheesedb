720a0e4fcd9e529a73fe0bf1f62bc740e508b4f3
Raffaele Magliulo <raffae.magliulo@gmail.com>
2026-08-13 09:47:54 +0100

    Add Bowtie2 mapping and SAM to BAM conversion script
    This script provides steps for mapping reads to assembled contigs using Bowtie2 and converting SAM to sorted BAM files.

---
+++ code/Bash/bowtie2.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# 3. Assembly-based
+
+## 3.2 Mapping of reads to assembled contigs
+
+### 3.2.1 Bowtie2 index
+bowtie2-build \
+    --threads 20 \
+    /path/to/SAMPLE_ID_contigs.fa \
+    /path/to/bowtie2_index/SAMPLE_ID
+
+### 3.2.2 Bowtie2 mapping
+bowtie2 \
+    -x /path/to/bowtie2_index/SAMPLE_ID \
+    -1 SAMPLE_ID_R1.fastq.bz2 \
+    -2 SAMPLE_ID_R2.fastq.bz2 \
+    -S SAMPLE_ID.sam \
+    -p 20 \
+    --very-sensitive-local \
+    --no-unal
+
+### 3.2.3 SAM to sorted BAM
+samtools view \
+    -b \
+    -@ 20 \
+    -o SAMPLE_ID.bam \
+    SAMPLE_ID.sam
+
+samtools sort \
+    -@ 20 \
+    -o SAMPLE_ID.sorted.bam \
+    SAMPLE_ID.bam
+
+samtools index \
+    -@ 20 \
+    SAMPLE_ID.sorted.bam
