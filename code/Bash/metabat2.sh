88c60d7e4b2a9c58f4498533280582b47d74b709
Raffaele Magliulo <raffae.magliulo@gmail.com>
2026-08-13 09:52:19 +0100

    Add MetaBAT2 script for assembly-based analysis

---
+++ code/Bash/metabat2.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# 3. Assembly-based
+
+## 3.3 MetaBAT2
+
+jgi_summarize_bam_contig_depths \
+    --outputDepth SAMPLE_ID_depth.txt \
+    SAMPLE_ID.sorted.bam
+
+metabat2 \
+    -m 1500 \
+    -i /path/to/SAMPLE_ID_contigs.fa \
+    -a SAMPLE_ID_depth.txt \
+    -o /path/to/output/SAMPLE_ID_metabat \
+    --unbinned \
+    --seed 0 \
+    -t 20
