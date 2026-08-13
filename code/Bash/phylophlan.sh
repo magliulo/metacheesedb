75185169491de6cbcffbc39261daed02569871db
Raffaele Magliulo <raffae.magliulo@gmail.com>
2026-08-13 10:04:34 +0100

    Add PhyloPhlAn script for assembly-based analysis

---
+++ code/Bash/phylophlan.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# 3. Assembly-based
+
+## 3.6 PhyloPhlAn
+
+phylophlan_write_default_configs.sh /path/to/configs
+
+phylophlan \
+    --input /path/to/filtered_MAGs \
+    --output /path/to/phylophlan_output \
+    -f /path/to/configs/supermatrix_aa.cfg \
+    --diversity high \
+    --fast \
+    --min_num_markers 50 \
+    -d phylophlan \
+    --genome_ext fa \
+    --nproc 20
