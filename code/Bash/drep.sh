c678022c97a5d8662d3095b92e6f548e780bbc6d
Raffaele Magliulo <raffae.magliulo@gmail.com>
2026-08-13 11:11:41 +0100

    Add dRep dereplication script

---
+++ code/Bash/drep.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# 3. Assembly-based
+
+## 3.6 dRep dereplication
+
+dRep dereplicate /path/to/drep_output \
+    --processors 20 \
+    --genomes /path/to/bins_path.txt \
+    --completeness 75 \
+    --contamination 10 \
+    --checkM_method lineage_wf \
+    --S_algorithm ANImf \
+    --MASH_sketch 1000 \
+    --P_ani 0.90 \
+    --S_ani 0.95 \
+    --cov_thresh 0.10
